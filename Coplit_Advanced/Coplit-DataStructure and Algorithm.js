114b0c71b37079a9b9b208d36ea0e8c4f3f4543a
nayoz301 <nayoz301@gmail.com>
2021-07-31 00:10:27 +0900

    :memo: DataStructure
    DataStructure

--- Coplit_Advanced/Coplit-DataStructure and Algorithm.js
+++ Coplit_Advanced/Coplit-DataStructure and Algorithm.js
@@ -1,21 +1,21 @@
-// const rockPaperScissors = function (num) {
-//   num = num || 3 //이게 뭐길래? 이거 없으면 안돌아감.
-//   let base = ['rock', 'paper', 'scissors'];
-//   let result = [];
-//   function howManyWays(storage, times){
-//     if(times===num){
-//       result.push(storage)
-//       return;
-//     }
-//     base.forEach(function(el){
-//       howManyWays(storage.concat(el), times+1)
-//     // for(let i = 0; i< base.length; i++){
-//     //   howManyWays(storage.concat(base[i]), times+1)
-//     })
-//   }
-//   howManyWays([], 0)
-//   return result;
-// };
+const rockPaperScissors = function (num) {
+  num = num || 3; //이게 뭐길래? 이거 없으면 안돌아감.
+  let base = ["rock", "paper", "scissors"];
+  let result = [];
+  function howManyWays(storage, times) {
+    if (times === num) {
+      result.push(storage);
+      return;
+    }
+    base.forEach(function (el) {
+      howManyWays(storage.concat(el), times + 1);
+      // for(let i = 0; i< base.length; i++){
+      //   howManyWays(storage.concat(base[i]), times+1)
+    });
+  }
+  howManyWays([], 0);
+  return result;
+};
 
 const rockPaperScissors = function (num) {
   num = num || 3;
