0440ea5a76b3f9e809a8734f105bacaa0aabf91e
nayoz301 <nayoz301@gmail.com>
2021-08-15 10:28:33 +0900

    :memo: Add toy4
    Add toy4

--- Coplit_Advanced/Coplit-DataStructure and Algorithm.js
+++ Coplit_Advanced/Coplit-DataStructure and Algorithm.js
@@ -102,3 +102,101 @@ const isSubsetOf = function (base, sample) {
   }
   return true;
 };
+
+//4
+//내 풀이
+const bubbleSort = function (arr) {
+  let storage = arr.slice();
+  let result = [];
+  let tempStorage = "";
+  let length = arr.length - 2;
+  for (let i = arr.length - 1; i >= 0; i--) {
+    let process = bubble(0, 0, i);
+    if (Array.isArray(process)) {
+      return process;
+    }
+  }
+  return result;
+
+  function bubble(numTill, count, i) {
+    if (count === i) {
+      return storage;
+    }
+    if (numTill === i) {
+      result = storage;
+      return;
+    }
+    if (storage[numTill] > storage[numTill + 1]) {
+      tempStorage = storage[numTill];
+      storage[numTill] = storage[numTill + 1];
+      storage[numTill + 1] = tempStorage;
+    } else {
+      count++;
+    }
+    let recur = bubble(numTill + 1, count, i);
+    return recur;
+  }
+};
+
+//래퍼런스
+const swap = function (idx1, idx2, arr) {
+  // 두 변수를 바꾸는 방법
+
+  // 1) 임시 변수를 활용한 방법
+  // let temp = arr[idx1];
+  // arr[idx1] = arr[idx2];
+  // arr[idx2] = temp;
+
+  // 2) Destructuring assignment를 활용한 방법
+  // arr이 reference type이라 가능
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+
+  // 3) XOR 연산을 활용한 방법
+  // arr이 reference type이라 가능
+  // arr[idx1] ^= arr[idx2];
+  // arr[idx2] ^= arr[idx1];
+  // arr[idx1] ^= arr[idx2];
+};
+
+// naive solution
+// let bubbleSort = function (arr) {
+//   let N = arr.length;
+
+//   for (let i = 0; i < N - 1; i++) {
+//     // 매 반복(iteration)마다 i번째로 큰 수가 마지막에서 i번째 위치하게 된다.
+//     // 이미 정렬된 요소는 고려할 필요가 없으므로, 'j < N - 1 - i'만 비교하면 된다.
+//     for (let j = 0; j < N - 1 - i; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         swap(j, j + 1, arr);
+//       }
+//     }
+//   }
+
+//   return arr;
+// };
+
+// optimized solution
+let bubbleSort = function (arr) {
+  let N = arr.length;
+
+  for (let i = 0; i < N; i++) {
+    // swap 횟수를 기록한다.
+    // 어떤 요소도 swap되지 않은 경우, 배열은 정렬된 상태이다.
+    let swaps = 0;
+
+    // 매 반복(iteration)마다 i번째로 큰 수가 마지막에서 i번째 위치하게 된다.
+    // 이미 정렬된 요소는 고려할 필요가 없으므로, 'j < N - 1 - i'만 비교하면 된다.
+    for (let j = 0; j < N - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swaps++;
+        swap(j, j + 1, arr);
+      }
+    }
+
+    if (swaps === 0) {
+      break;
+    }
+  }
+
+  return arr;
+};
