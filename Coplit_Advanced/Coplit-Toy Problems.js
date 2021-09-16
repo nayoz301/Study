fd99e968e712fa9096f0e0d316b31d25b085d265
nayoz301 <nayoz301@gmail.com>
2021-09-16 23:15:24 +0900

    :memo: Add Toy Tree
    Add Toy Tree

--- Coplit_Advanced/Coplit-Toy Problems.js
+++ Coplit_Advanced/Coplit-Toy Problems.js
@@ -666,3 +666,31 @@ const binarySearch = function (arr, target) {
   }
   return -1;
 };
+
+//Q12 Tree BFS
+let bfs = function (node) {
+  let queue = [node];
+  const values = [];
+  while (queue.length > 0) {
+    const head = queue[0];
+    queue = queue.slice(1);
+
+    values.push(head.value);
+
+    head.children.forEach((child) => queue.push(child));
+  }
+  return values;
+};
+
+//여기는 건드릴 필요없음
+let Node = function (value) {
+  this.value = value;
+  this.children = [];
+};
+
+// 위 Node 객체로 구성되는 트리는 매우 단순한 형태의 트리입니다.
+// membership check(중복 확인)를 따로 하지 않습니다.
+Node.prototype.addChild = function (child) {
+  this.children.push(child);
+  return child;
+};
