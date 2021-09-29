40f076b38d4a6a9cbc05c9ef740875d609a551d1
nayoz301 <nayoz301@gmail.com>
2021-09-29 18:52:30 +0900

    :memo: Add Toy Q22
    Add Toy Q22

--- Coplit_Advanced/Coplit-Toy Problems.js
+++ Coplit_Advanced/Coplit-Toy Problems.js
@@ -968,7 +968,6 @@ const merge = function (left, right) {
   // return results;
 };
 
-//Q21
 const mergeSort = function (arr) {
   // ending condition: length === 1 인 배열이 들어올 때, 정렬이 끝난 것.
   if (arr.length === 1) return arr;
@@ -984,6 +983,7 @@ const mergeSort = function (arr) {
   return merge(mergeSort(left), mergeSort(right));
 };
 
+//Q21
 const inequalityNumber = function (signs) {
   const aux = (idx, signs, nums, digits, isVisited) => {
     if (idx === signs.length) {
@@ -1027,4 +1027,33 @@ const inequalityNumber = function (signs) {
   const min = aux(-1, signs, [], digits, Array(10).fill(false));
   const max = aux(-1, signs, [], digits.reverse(), Array(10).fill(false));
   return max - min;
+};
+
+//Q22
+const rotateMatrix = function (matrix, rotateNum = 1) {
+  // rotateNum 이 0일 수 있으므로 아래와 같은 초기화는 지양해야 함
+  // rotateNum = rotateNum || 1
+  const N = matrix.length;
+  // 빈 배열을 입력받을 수 있다.
+  const M = matrix[0] && matrix[0].length;
+
+  rotateNum = rotateNum % 4;
+  // 회전하지 않는다.
+  if (rotateNum === 0) return matrix;
+
+  const rotated = [];
+  // 홀수번 회전 시 M x N, 짝수번 회전 시 N x M
+  const RC = rotateNum % 2 === 1 ? [M, N] : [N, M];
+
+  for (let row = 0; row < RC[0]; row++) {
+    rotated[row] = [];
+    for (let col = 0; col < RC[1]; col++) {
+      if (rotateNum === 1) rotated[row][col] = matrix[N - col - 1][row];
+      else if (rotateNum === 2)
+        rotated[row][col] = matrix[N - row - 1][M - col - 1];
+      else rotated[row][col] = matrix[col][M - row - 1];
+    }
+  }
+
+  return rotated;
 };
