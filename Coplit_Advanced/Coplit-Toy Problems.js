8c8a14e092f2c7c57e337a3b4a0851702cef2808
nayoz301 <nayoz301@gmail.com>
2021-10-04 23:52:55 +0900

    :memo: Add Toy Q26
    Add Toy Q26

--- Coplit_Advanced/Coplit-Toy Problems.js
+++ Coplit_Advanced/Coplit-Toy Problems.js
@@ -1222,3 +1222,22 @@ const robotPath = function (room, src, dst) {
   const [r, c] = dst;
   return room[r][c] - 1;
 };
+
+//Q26
+// dynamic programming: O(N)
+const LSCS = function (arr) {
+  let subArrSum = 0; // 연속 배열의 합
+  let max = Number.MIN_SAFE_INTEGER; // 정답의 후보를 저장
+  for (let i = 0; i < arr.length; i++) {
+    subArrSum = subArrSum + arr[i];
+    if (subArrSum > max) max = subArrSum;
+
+    // 연속된 구간의 합이 음수인 경우,
+    // 해당 부분은 버리고 다시 시작해도 된다.
+    if (subArrSum < 0) {
+      subArrSum = 0;
+    }
+  }
+
+  return max;
+};
