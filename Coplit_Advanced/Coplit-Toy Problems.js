2094a380885a6719fd89003057e0e9ae0d885692
nayoz301 <nayoz301@gmail.com>
2021-10-08 17:10:37 +0900

    :memo: Add Toy Q29
    Add Toy Q29

--- Coplit_Advanced/Coplit-Toy Problems.js
+++ Coplit_Advanced/Coplit-Toy Problems.js
@@ -1413,3 +1413,47 @@ const robotPath2 = function (room, src, sDir, dst, dDir) {
 
   return dist[dRow][dCol];
 };
+
+//Q29
+function swap(idx1, idx2, arr) {
+  // 두 변수를 바꾸는 방법
+
+  // 1) 임시 변수를 활용한 방법
+  // let temp = arr[idx1];
+  // arr[idx1] = arr[idx2];
+  // arr[idx2] = temp;
+
+  // 2) Destructuring assignment를 활용한 방법
+  // arr이 reference type이라 가능
+  [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+
+  // 3) XOR 연산을 활용한 방법
+  // arr이 reference type이라 가능
+  // arr[idx1] ^= arr[idx2];
+  // arr[idx2] ^= arr[idx1];
+  // arr[idx1] ^= arr[idx2];
+}
+
+function getParentIdx(idx) {
+  // TODO: 여기에 코드를 작성합니다.
+  return Math.floor((idx - 1) / 2);
+}
+
+function insert(heap, item) {
+  // TODO: 여기에 코드를 작성합니다.
+  heap.push(item);
+  let curIdx = heap.length - 1;
+  let pIdx = getParentIdx(curIdx);
+  while (pIdx >= 0 && heap[curIdx] > heap[pIdx]) {
+    swap(curIdx, pIdx, heap);
+    curIdx = pIdx;
+    pIdx = getParentIdx(curIdx);
+  }
+  return heap;
+}
+
+const binaryHeap = function (arr) {
+  return arr.reduce((heap, item) => {
+    return insert(heap, item);
+  }, []);
+};
