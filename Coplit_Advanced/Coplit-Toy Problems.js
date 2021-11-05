95bbbce84c3ed16184865bc73fb4fec01162242e
nayoz301 <nayoz301@gmail.com>
2021-11-05 23:58:27 +0900

    :memo: Add Toy Q45
    Add Toy Q45

--- Coplit_Advanced/Coplit-Toy Problems.js
+++ Coplit_Advanced/Coplit-Toy Problems.js
@@ -2285,3 +2285,21 @@ const TSP = function (places) {
 
   return currentMinDist;
 };
+
+//Q45 subset O(2^N)
+const subsetSum = function (set, bound) {
+  const LENGTH = set.length;
+  function pickOrNot(idxToCheck, left) {
+    if (idxToCheck === LENGTH) return bound - left;
+
+    if (set[idxToCheck] > left) return pickOrNot(idxToCheck + 1, left);
+    if (set[idxToCheck] === left) return bound;
+
+    return Math.max(
+      pickOrNot(idxToCheck + 1, left - set[idxToCheck]),
+      pickOrNot(idxToCheck + 1, left)
+    );
+  }
+
+  return pickOrNot(0, bound);
+};
